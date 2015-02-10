67f8937b61c0cec0a76e95bf312bafb79e14fc79
Wissocq Sarah <wissocq@a13p3.fil.univ-lille1.fr>
2015-02-10 17:43:30 +0100

    Correct the previous question and try to do the diagonal sudoku

--- tp3_wissocq_petit.pl
+++ tp3_wissocq_petit.pl
@@ -1,4 +1,5 @@
-/* PETIT Antoine & WISSOCQ Sarah */
+/* PETIT Antoine & WISSOCQ Sarah 
+Pour afficher liste en entier: set_prolog_flag(prompt_alternatives_on,groundless), après l'appel appuyer sur w*/
 :- use_module(library(clpfd)).
 
 grille([[_,_,_,_,_,_,_,_,_],
@@ -33,7 +34,7 @@ verifier([]).
 verifier(X) :- X ins 1..9.
 
 verifie([]).
-verifie([X|XS]) :- verifier(X), all_different(X), verifie(XS).
+verifie([X|XS]) :- verifier(X), all_distinct(X), verifie(XS).
 
 /*Question 6:*/
 eclate([],_,[]).
@@ -50,7 +51,17 @@ decoupe([X1,Y1,Z1|XS],[X2,Y2,Z2|YS],[X3,Y3,Z3|ZS],[[X1,Y1,Z1,X2,Y2,Z2,X3,Y3,Z3]|
 
 /*Question 9:*/
 carres([],[]).
-carres([X,Y,Z|XS],[L2|L]) :- decoupe(X,Y,Z,L2), carres(XS,L).
+carres([X,Y,Z|XS],[R1,R2,R3|L]) :- decoupe(X,Y,Z,[R1,R2,R3]), carres(XS,L).
 
 /*Question 10:*/
-solution(S):-  bonnetaille(S,9),verifie(CS),bonnetaille(TS,9), verifie(S),verifie(TS),transp(S,TS), carres(S,CS).
+solution(S):- transp(S,TS), carres(S,CS), bonnetaille(S,9),verifie(CS),bonnetaille(TS,9), verifie(S),verifie(TS).
+
+/*Question 11:*/
+elem([X|_],1,X).
+elem([_|XS],N,R) :- N1 is (N-1), elem(XS,N1,R).
+
+/* diagonale calcule la diagonale de en haut à droite vers en bas à gauche*/
+diagonale([],_,[]).
+diagonale([X|XS],Y,[R|L]) :- Y1 is (Y-1),  elem(X,Y,R), diagonale(XS,Y1,L).
+
+solutiondiag(S):- transp(S,TS), carres(S,CS), bonnetaille(S,9),verifie(CS),bonnetaille(TS,9), verifie(S),verifie(TS), diagonale(S,9,D1), diagonale(TS,9,D2), verifie([D1]), verifie([D2]).
